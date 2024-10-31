6b6c066751d8efba12116323d5199da1b05b90d1
Moyo Fagbuyi <rfagbuyi@gmail.com>
2024-10-31 11:57:05 -0400

    registering should lead to homepage, untested though

--- app/__init__.py
+++ app/__init__.py
@@ -7,28 +7,28 @@ app.secret_key = tiny.make()
 
 @app.route("/",  methods=['GET','POST'])
 def disp_loginpage():
-    if 'username' and 'password' in session:
-        user = session['username']
-        return render_template('homepage.html', name = user)
-    return render_template( 'login.html' )
+    if 'email' and 'password' in session:
+        user = session['email']
+        return render_template('login.html', name = user)
+    return render_template( 'register.html' )
 
 
 @app.route("/auth", methods=['POST'])
 def post():
-    session['username'] = request.form['username']
+    session['email'] = request.form['email']
     session['password'] = request.form['password']
-    user = session['username']
+    user = session['email']
     return render_template('homepage.html', name = user)
 
 @app.route("/logout", methods = ['GET', 'POST'])
 def logout():
-    session.pop('username', None)
+    session.pop('email', None)
     session.pop('password', None)
     return render_template('logout.html')
 
 @app.route("/homepage", methods = ['GET', 'POST'])
 def redirect():
-    return render_template('login.html')
+    return render_template('homepage.html')
 
 
 if __name__ == "__main__":
