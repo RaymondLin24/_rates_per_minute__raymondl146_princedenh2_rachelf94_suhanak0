3c12aa302e7b0dc480523067e4e4536d17a3ab7e
Moyo Fib <rfagbuyi@gmail.com>
2024-11-06 16:05:31 -0500

    removed bad merges sorry my falut

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect, url_of #this one stores like everything
+from flask import Flask, render_template, request, session #this one stores like everything
 import os
 import tiny, db_helpers
 app = Flask(__name__)
@@ -7,38 +7,11 @@ app.secret_key = tiny.make()
 
 @app.route("/",  methods=['GET'])
 def register():
-    if 'email' and 'password' in session:
-        user = session['email']
+    if 'username' and 'password' in session:
+        user = session['username']
         # pwd = session['password']
         # db_helpers.add_user(user, pwd)
         return render_template('homepage.html', name = user)
-    return render_template( 'register.html' )
-
-
-@app.route("/login",  methods=['GET','POST'])
-def disp_loginpage():
-    if 'email' and 'password' in session:
-        user = session['email']
-        return render_template('homepage.html', name = user)
-    return render_template( 'login.html' ) #renders homepage
-
-
-@app.route("/logout", methods = ['GET', 'POST'])
-def logout():
-    session.pop('email', None)
-    session.pop('password', None)
-    return render_template('logout.html')
-
-
-@app.route("/homepage", methods = ['GET', 'POST'])
-def redirect():
-    session['username'] = request.form['username']
-    session['password'] = request.form['password']
-    user = session['email']
-    pwd = session['password']
-    db_helpers.add_user(user, pwd)
-    return render_template('homepage.html', name = user)
-
     return render_template( 'register.html' )
 
 
@@ -57,7 +30,32 @@ def logout():
     return render_template('logout.html')
 
 
->>>>>>> 9c5b938a8080ccf733dc35ff8f7ab9c43c2d12a4
+@app.route("/homepage", methods = ['GET', 'POST'])
+def redirect():
+    session['username'] = request.form['username']
+    session['password'] = request.form['password']
+    user = session['username']
+    # pwd = session['password']
+    # db_helpers.add_user(user, pwd)
+    return render_template('homepage.html', name = user)
+
+    return render_template( 'register.html' )
+
+
+# @app.route("/login",  methods=['GET','POST'])
+# def disp_loginpage():
+#     if 'username' and 'password' in session:
+#         user = session['username']
+#         return render_template('homepage.html', name = user)
+#     return render_template( 'login.html' ) #renders homepage
+
+
+# @app.route("/logout", methods = ['GET', 'POST'])
+# def logout():
+#     session.pop('username', None)
+#     session.pop('password', None)
+#     return render_template('logout.html')
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
