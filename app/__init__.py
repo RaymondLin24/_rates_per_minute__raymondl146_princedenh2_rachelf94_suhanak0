18e83adec63926b92821bb2c93651853c8a851ea
Raymond Lin <rlin51@stuy.edu>
2024-11-06 11:44:57 -0500

    imported database functions

--- app/__init__.py
+++ app/__init__.py
@@ -1,39 +1,39 @@
-from flask import Flask, render_template, request, session #this one stores like everything
+from flask import Flask, render_template, request, session, redirect, url_of #this one stores like everything
 import os
 import tiny
+from database import test_populate, add_user, check_username, add_story, add_contribution, get_story, contributed_to_story, correct_login, get_user_id
 app = Flask(__name__)
 app.secret_key = tiny.make()
 
+@app.route("/homepage", methods = ['GET', 'POST'])
+def redirect():
+    session['username'] = request.form['username']
+    session['password'] = request.form['password']
+    user = session['email']
+    return render_template('homepage.html', name = user)
 
-@app.route("/",  methods=['GET'])
+
+@app.route("/",  methods=['GET', 'POST'])
 def register():
-    if 'email' and 'password' in session:
-        user = session['email']
+    if 'username' and 'password' in session:
+        user = session['username']
         return render_template('homepage.html', name = user)
     return render_template( 'register.html' )
 
 
 @app.route("/login",  methods=['GET','POST'])
 def disp_loginpage():
-    if 'email' and 'password' in session:
-        user = session['email']
+    if 'username' and 'password' in session:
+        user = session['username']
         return render_template('homepage.html', name = user)
     return render_template( 'login.html' ) #renders homepage
 
 
 @app.route("/logout", methods = ['GET', 'POST'])
 def logout():
-    session.pop('email', None)
+    session.pop('username', None)
     session.pop('password', None)
     return render_template('logout.html')
-
-
-@app.route("/homepage", methods = ['GET', 'POST'])
-def redirect():
-    session['email'] = request.form['email']
-    session['password'] = request.form['password']
-    user = session['email']
-    return render_template('homepage.html', name = user)
 
 
 if __name__ == "__main__":
